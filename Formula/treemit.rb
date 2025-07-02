5a8b0251965ea2251ba8669ceffdc3ed4da02fee
Nagaemonn <nagaemonn20020811@icloud.com>
2025-07-02 13:29:34 +0900

    Update Treemit formula with new release URLs and SHA256 checksums for ARM and Intel architectures

--- Formula/treemit.rb
+++ Formula/treemit.rb
@@ -1,16 +1,19 @@
 class Treemit < Formula
   desc "A CLI tool for emitting tree structures"
   homepage "https://github.com/Nagaemonn/treemit"
-  version "0.1.2-beta"
+  # ↓↓↓ 新しいリリースを出したら、下記3か所を必ず更新！
+  version "0.1.2-beta" # ← 新しいバージョンに変更
   license "MIT"
 
   on_macos do
     on_arm do
+      # ↓ 新しいリリースのURLとsha256に変更
       url "https://github.com/Nagaemonn/treemit/releases/download/v0.1.2-beta/treemit-darwin-arm64.tar.gz"
       sha256 "ed105ea822ae93651d4e9ea0989d82798bceb1728fe8ee441e13e9a9194ccd82"
     end
 
     on_intel do
+      # ↓ 新しいリリースのURLとsha256に変更
       url "https://github.com/Nagaemonn/treemit/releases/download/v0.1.2-beta/treemit-darwin-amd64.tar.gz"
       sha256 "6f3a31cbbf2973641a402f78a2965ebf2f41bba99e51aebd2278930b0fe3fd02"
     end
