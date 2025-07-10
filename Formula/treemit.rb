23e9ca8ee528bf1cb9dcd35a4a861cb798f5847f
Nagaemonn <nagaemonn20020811@icloud.com>
2025-07-11 01:48:34 +0900

    Update Treemit formula to version 0.4.2 with new release URLs and SHA256 checksums for ARM and Intel architectures

--- Formula/treemit.rb
+++ Formula/treemit.rb
@@ -2,20 +2,20 @@ class Treemit < Formula
   desc "A CLI tool for emitting tree structures"
   homepage "https://github.com/Nagaemonn/treemit"
   # ↓↓↓ 新しいリリースを出したら、下記3か所を必ず更新！
-  version "0.4.0" # ← 新しいバージョンに変更
+  version "0.4.2" # ← 新しいバージョンに変更
   license "MIT"
 
   on_macos do
     on_arm do
       # ↓ 新しいリリースのURLとsha256に変更
-      url "https://github.com/Nagaemonn/treemit/releases/download/v0.4.0/treemit-darwin-arm64.tar.gz"
-      sha256 "e75e945b71cf1d557511b53877b3a9b8c389240f6f8a19e84c6c6aa547ef2aeb"
+      url "https://github.com/Nagaemonn/treemit/releases/download/v0.4.2/treemit-darwin-arm64.tar.gz"
+      sha256 "sha256:bb3e4703e1a0406e54e06d51c0d188cd12f1c9e5e4c7b08c9e7ff278fc7718f9"
     end
 
     on_intel do
       # ↓ 新しいリリースのURLとsha256に変更
-      url "https://github.com/Nagaemonn/treemit/releases/download/v0.4.0/treemit-darwin-amd64.tar.gz"
-      sha256 "b3af46f6f712c095d7e465b9044d617ceb44d80b27b77e97aa1b4ae12ec09e34"
+      url "https://github.com/Nagaemonn/treemit/releases/download/v0.4.2/treemit-darwin-amd64.tar.gz"
+      sha256 "sha256:d1156e1ae7386cdfd413f9a072081bd5459652d6d2514f8fdc3a7c1659a6de80"
     end
   end
 
